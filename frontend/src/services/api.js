d8bae6f300a9508b6e4296d3ce9da21b43aa832c
sj005cloud <sjoshi_be23@thapar.edu>
2026-06-05 18:56:03 +0530

    fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://drug-admet-predictor-production.up.railway.app/";
+const API_BASE_URL = "https://drug-admet-predictor-production.up.railway.app";
 
 export const predictMolecule = async (smiles) => {
   const response = await axios.post(
