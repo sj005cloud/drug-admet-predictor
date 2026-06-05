d6b342cbe63d20a2f963164c77aedb439c2e201e
sj005cloud <sjoshi_be23@thapar.edu>
2026-06-05 18:23:45 +0530

    prepare frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://drug-admet-predictor-production.up.railway.app/";
 
 export const predictMolecule = async (smiles) => {
   const response = await axios.post(
