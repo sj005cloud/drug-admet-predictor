b133a208acfc3eb525d8000c71b6ef84cb4fd2f5
sj005cloud <sjoshi_be23@thapar.edu>
2026-06-06 11:09:38 +0530

    ui improvement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,7 +83,7 @@ setInterpretation(result.interpretation || "");
 
           <div className="title-row">
   <img
-    src="/logo.png"
+    src="/logo1.PNG"
     alt="Logo"
     className="site-logo"
   />
